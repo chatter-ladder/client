6a081f81e64a0586f59949dd22049f0362e95b20
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-07 11:03:47 +0100

    Project setup and added a Flashcard component

---
+++ src/components/Flashcard/Flashcard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Flashcard.module.css';
+
+const Flashcard = () => {
+  return(
+    <div className={classes.Flashcard}>
+      <h4>hola</h4>
+    </div>
+  );
+}
+
+export default Flashcard;
---
+++ src/containers/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Flashcard from '../components/Flashcard/Flashcard';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Flashcard />
+      </div>
+    );
+  }
+}
+
+export default App;
