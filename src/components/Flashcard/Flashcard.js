1a27f04199e33e6721ee232203a8ddaebe532b5f
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-07 14:33:51 +0100

    Card flips when clicked

--- src/components/Flashcard/Flashcard.js
+++ src/components/Flashcard/Flashcard.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Flashcard.module.css';
 
-const Flashcard = () => {
-  return(
-    <div className={classes.Flashcard}>
-      <div className={classes.Pattern}>
-        <div className={classes.Content}>
-          <h4>hola</h4>
+class Flashcard extends Component {
+
+  state = {
+    flipped: false,
+  }
+
+  flipHandler = () => {
+    this.setState(prevState => ({
+      flipped: !prevState.flipped
+    }));
+  };
+
+  containerClasses = () => {
+    if (this.state.flipped) {
+      return `${classes.Flashcard} ${classes.Flipped}`
+    }
+    return classes.Flashcard
+  }
+
+  render() {
+    return(
+      <div className={classes.Container}>
+        <div className={this.containerClasses()} onClick={this.flipHandler}>
+          <div className={`${classes.Pattern} ${classes.Front}`}>
+            <h4>Front</h4>
+          </div>
+          <div className={`${classes.Pattern} ${classes.Back}`}>
+            <h4>Back</h4>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  };
+};
 
 export default Flashcard;
