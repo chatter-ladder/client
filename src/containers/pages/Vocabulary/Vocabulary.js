1c7bf8955992e3124ddc0c2f12227f4bbb85c240
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-11 19:13:18 +0100

    Added basic form and basic table structure to Vobabulary page

--- src/containers/pages/Vocabulary/Vocabulary.js
+++ src/containers/pages/Vocabulary/Vocabulary.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const Vocabulary = () => {
   return (
-      <h1>Vocabulary page</h1>
+      <>
+      <h1>Vocabulary List</h1>
+      <form>
+        <input type='text' name='word' placeholder="Enter new word" />
+        <input type='text' name='translation' placeholder="Enter translation" />
+        <button>Submit</button>
+      </form>
+
+      <table>
+        <tbody>
+          <tr>
+            <th>Word</th>
+            <th>Translation</th>
+            <th>Group</th>
+            <th>Learning Status</th>
+          </tr>
+        </tbody>
+      </table>
+      </>
   )
 }
 
