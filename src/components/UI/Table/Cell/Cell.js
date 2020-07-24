14f401ff1649e1d83f2942c32e8bdb67e06e48d3
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-24 15:53:30 +0100

    Refactored Cell to handle styles in one function rather than two

--- src/components/UI/Table/Cell/Cell.js
+++ src/components/UI/Table/Cell/Cell.js
@@ -4,37 +4,26 @@ import classes from './Cell.module.css';
 
 const Cell = (props) => {
 
-  let headerStyle = () => {
-    let style = [classes.Header]
-    if (props.fixed) {
-      style.push(classes.FixedCell)
+  let cellStyle = () => {
+    let style = [];
+    if (props.header) {
+      style = [classes.Header]
     } else {
-      style.push(classes.Body)
+      style = [classes.Content]
     }
-    return style.join(" ")
-  }
 
-  let contentStyle = () => {
-    let style = [classes.Content]
     if (props.fixed) {
       style.push(classes.FixedCell)
     } else {
       style.push(classes.Body)
     }
-    return style.join(" ")
+    return style.join(" ");
   }
  
   return (
-    props.header
-    ? (
-      <th className={headerStyle()}>
+      <th className={cellStyle()}>
         {props.content}
       </th>
-    ) : (
-      <th className={contentStyle()}>
-        {props.content}
-      </th>
-    )
   );
 }
 
