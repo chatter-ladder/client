f95eefd04355eea88cf1ef446b7f2b28fa7e503e
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-24 15:37:05 +0100

    Created a cellHeights state in Table and added a ref to keep track of rows max height

--- src/components/UI/Table/Table.js
+++ src/components/UI/Table/Table.js
@@ -5,6 +5,18 @@ import Cell from './Cell/Cell';
 
 class Table extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      cellHeights: []
+    }
+    this.tableRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.cellHeightResizeHandler();
+  }
+
   renderHeadingRow = (content, colIndex) => {
     return (
       <Cell
@@ -12,21 +24,44 @@ class Table extends Component {
         content={content}
         header={true}
         fixed={colIndex === 0}
+        // height={this.cellHeight[0]}
       />
     )
   }
 
-  renderBodyRow = (content, rowIndex, contentIndex) => {
+  renderBodyRow = (row, rowIndex) => {
     // console.log(rowIndex)
     // console.log(contentIndex)
     return (
-      <Cell
-        key={`${rowIndex}-${contentIndex}`}
-        content={content}
-        header={false}
-        fixed={contentIndex === 0}
-      />
+      <tr key={rowIndex}>
+        {row.map((content, contentIndex) => {
+          return (
+            <Cell
+              key={`${rowIndex}-${contentIndex}`}
+              content={content}
+              header={false}
+              fixed={contentIndex === 0}
+            />
+          )
+        })}
+      </tr>
     )
+  }
+
+  getTallestCellHeights = () => {
+    const rows = Array.from(this.tableRef.current.getElementsByTagName('tr'));
+    let {heights} = this.state;
+    
+    (heights = rows.map((row) => {
+      const fixedCell = (row.childNodes)[0];
+      return Math.max(row.clientHeight, fixedCell.clientHeight);
+    }));
+
+    return heights;
+  }
+
+  cellHeightResizeHandler = () => {
+    this.setState({cellHeights: this.getTallestCellHeights()});
   }
 
   render () {
@@ -36,20 +71,22 @@ class Table extends Component {
       </tr>
     )
 
-    const bodyRows = (
-      this.props.rows.map((row, rowIndex) => {
-        return (
-          <tr key={rowIndex}>
-            {row.map((content, contentIndex) => this.renderBodyRow(content, rowIndex, contentIndex))}
-          </tr>
-        )
-      })
-    );
+    const bodyRows = this.props.rows.map(this.renderBodyRow)
+
+    // const bodyRows = (
+    //   this.props.rows.map((row, rowIndex) => {
+    //     return (
+    //       <tr key={rowIndex}>
+    //         {row.map((content, contentIndex) => this.renderBodyRow(content, rowIndex, contentIndex))}
+    //       </tr>
+    //     )
+    //   })
+    // );
 
     return (
       <div className={classes.Container}>
         <div className={classes.ScrollContainer}>
-          <table className={classes.Table}>
+          <table className={classes.Table} ref={this.tableRef}>
             <thead>{headingRows}</thead>
             <tbody>{bodyRows}</tbody>
           </table>
