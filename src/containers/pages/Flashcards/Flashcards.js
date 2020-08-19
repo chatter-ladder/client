9c4771f2a7f1d74402876aa78a0f038dbeac3869
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-19 18:49:07 +0100

    Added buttons and default number of flashcards to see

--- src/containers/pages/Flashcards/Flashcards.js
+++ src/containers/pages/Flashcards/Flashcards.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Flashcard from '../../../components/Flashcard/Flashcard';
 
-const Flashcards = () => {
-  return (
-    <>
-      <h1>Flashcards page</h1>
-      <Flashcard />
-    </>
-  )
+class Flashcards extends Component {
+
+  state = {
+    numFlashcards: 5
+  }
+
+  render () {
+    return (
+      <>
+        <h1>Flashcards page</h1>
+    <div>{this.state.numFlashcards}</div>
+        <button>+</button>
+        <button>-</button>
+        <Flashcard />
+      </>
+    )
+  }
 }
 
 export default Flashcards;
