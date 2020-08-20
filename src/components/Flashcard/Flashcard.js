db805831fdac87323ee52e97c08bd1bb0ed91aa1
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-20 17:44:59 +0100

    Converted Flashcard component to a functional component

--- src/components/Flashcard/Flashcard.js
+++ src/components/Flashcard/Flashcard.js
@@ -1,37 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Flashcard.module.css';
 
-class Flashcard extends Component {
+const Flashcard = (props) => {
 
-  flashcardClasses = () => {
+  const flashcardClasses = () => {
     let updatedClasses = [ classes.Flashcard ]
-    if (this.props.flipped ) {
+    if (props.flipped ) {
       updatedClasses.push(classes.Flipped)
     }
-    if (this.props.transition) {
+    if (props.transition) {
       updatedClasses.push(classes.Transition)
     }
     return updatedClasses.join(" ");
   }
 
-  render() {
-    return(
-      <div className={classes.Container}>
-        <div className={this.flashcardClasses()} onClick={this.props.clicked}>
-          <div className={`${classes.Pattern} ${classes.Front}`}>
-            <div className={classes.Content}>
-              <h4>{this.props.front}</h4>
-            </div>
+  return(
+    <div className={classes.Container}>
+      <div className={flashcardClasses()} onClick={props.clicked}>
+        <div className={`${classes.Pattern} ${classes.Front}`}>
+          <div className={classes.Content}>
+            <h4>{props.front}</h4>
           </div>
-          <div className={`${classes.Pattern} ${classes.Back}`}>
-            <div className={classes.Content}>
-              <h4>{this.props.back}</h4>
-            </div>
+        </div>
+        <div className={`${classes.Pattern} ${classes.Back}`}>
+          <div className={classes.Content}>
+            <h4>{props.back}</h4>
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
 };
 
 export default Flashcard;
