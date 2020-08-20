24972cf94e76d5d3d83aecd2d8dbcdc653d27528
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-20 12:45:30 +0100

    Added constants for minimum and maximum flashcards

--- src/containers/pages/Flashcards/Flashcards.js
+++ src/containers/pages/Flashcards/Flashcards.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 
 import Flashcard from '../../../components/Flashcard/Flashcard';
 
+const MINIMUM_FLASHCARDS = 5;
+const MAXIMUM_FLASHCARDS = 30;
+
 class Flashcards extends Component {
 
   state = {
-    numFlashcards: 5
+    numFlashcards: MINIMUM_FLASHCARDS
+  }
+
+  componentDidMount () {
+    // fetch from server the number of vocab words user has in database
   }
 
   increaseFlashcardsHandler = () => {
-    if (this.state.numFlashcards < 30) {
+    if (this.state.numFlashcards < MAXIMUM_FLASHCARDS) {
       this.setState(prevState => {
         return { numFlashcards: prevState.numFlashcards + 1 }
       })
@@ -17,7 +24,7 @@ class Flashcards extends Component {
   }
 
   decreaseFlashcardsHandler = () => {
-    if (this.state.numFlashcards > 5) {
+    if (this.state.numFlashcards > MINIMUM_FLASHCARDS) {
       this.setState(prevState => {
         return { numFlashcards: prevState.numFlashcards - 1 }
       })
