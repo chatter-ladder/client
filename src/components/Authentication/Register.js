06d74cf6fc79975f8cffc403ca1d28034c84a1da
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-27 17:29:49 +0100

    Set up a basic registration form to create new users

---
+++ src/components/Authentication/Register.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+import Input from '../UI/Input/Input';
+
+class Register extends Component {
+
+    state = {
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    }
+
+    onChangeHandler = (event, formElement) => {
+        this.setState({ 
+            ...this.state,
+            [formElement]: event.target.value 
+        })
+    }
+
+    render () {
+        return (
+            <form>
+                <Input 
+                    className='placeholder'
+                    type='text'
+                    name='username'
+                    placeholder='Enter a username'
+                    value={this.state.username}
+                    onChange={(event) => this.onChangeHandler(event, 'username')}
+                />
+                                <Input 
+                    className='placeholder'
+                    type='email'
+                    name='email'
+                    placeholder='Enter email address'
+                    value={this.state.email}
+                    onChange={(event) => this.onChangeHandler(event, 'email')}
+                />
+                                <Input 
+                    className='placeholder'
+                    type='password'
+                    name='password'
+                    placeholder='Enter password'
+                    value={this.state.password}
+                    onChange={(event) => this.onChangeHandler(event, 'password')}
+                />
+                                <Input 
+                    className='placeholder'
+                    type='password'
+                    name='confirmPassword'
+                    placeholder='Confirm password'
+                    value={this.state.confirmPassword}
+                    onChange={(event) => this.onChangeHandler(event, 'confirmPassword')}
+                />
+            </form>
+        )
+    }
+};
+
+export default Register;
--- src/containers/pages/Home/Home.js
+++ src/containers/pages/Home/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import Register from '../../../components/Authentication/Register';
+
 const Home = () => {
   return (
-    <h1>Home page</h1>
+    <>
+      <h1>Home page</h1>
+      <Register />
+    </>
   )
 }
 
