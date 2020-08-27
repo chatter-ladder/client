15eb1a64a4c74372de48e5d97b1a418e509ae9e6
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-27 17:55:39 +0100

    Added forgotten password link (without functionality

--- src/containers/pages/Authentication/Login.js
+++ src/containers/pages/Authentication/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 import Input from '../../../components/UI/Input/Input';
 
@@ -18,24 +19,27 @@ class Register extends Component {
 
     render () {
         return (
-            <form>
-                <Input 
-                    className='placeholder'
-                    type='email'
-                    name='email'
-                    placeholder='Enter email address'
-                    value={this.state.email}
-                    onChange={(event) => this.onChangeHandler(event, 'email')}
-                />
-                                <Input 
-                    className='placeholder'
-                    type='password'
-                    name='password'
-                    placeholder='Enter password'
-                    value={this.state.password}
-                    onChange={(event) => this.onChangeHandler(event, 'password')}
-                />
-            </form>
+            <>
+                <form>
+                    <Input 
+                        className='placeholder'
+                        type='email'
+                        name='email'
+                        placeholder='Enter email address'
+                        value={this.state.email}
+                        onChange={(event) => this.onChangeHandler(event, 'email')}
+                    />
+                                    <Input 
+                        className='placeholder'
+                        type='password'
+                        name='password'
+                        placeholder='Enter password'
+                        value={this.state.password}
+                        onChange={(event) => this.onChangeHandler(event, 'password')}
+                    />
+                </form>
+                <Link>Forgotten password?</Link>
+            </>
         )
     }
 };
