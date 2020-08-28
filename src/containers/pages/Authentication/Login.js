2d6bfb723038a5efeb0d288f37b687eb60b20e55
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-28 13:58:48 +0100

    Added checkValidity handler to Login and updated validity checks in state for email and password

--- src/containers/pages/Authentication/Login.js
+++ src/containers/pages/Authentication/Login.js
@@ -10,7 +10,8 @@ class Login extends Component {
             email: {
                 value: '',
                 validation: {
-                    required: true
+                    required: true,
+                    isEmail: true
                 },
                 valid: false,
                 touched: false
@@ -18,12 +19,39 @@ class Login extends Component {
             password: {
                 value: '',
                 validation: {
-                    required: true
+                    required: true,
+                    minLength: 6
                 },
                 valid: false,
                 touched: false
             }
         }
+    }
+
+    checkValidity = (value, rules) => {
+        let isValid = true;
+
+        if (!rules) {
+            return true
+        }
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.isEmail) {
+            const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            isValid = emailRegex.test(value.toLowerCase());
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+        return isValid
     }
 
     onChangeHandler = (event, formElement) => {
