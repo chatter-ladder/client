2fad97d8cb47c54fcf94ead11db18e0675db8a89
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-03 18:13:38 +0100

    Connected vocabulary to redux store, updated get vocabulary to use axios, and passed accessToken as authorization header to server

--- src/containers/pages/Vocabulary/Vocabulary.js
+++ src/containers/pages/Vocabulary/Vocabulary.js
@@ -1,60 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
 
-import Table from '../../../components/UI/Table/Table';
-import Input from '../../../components/UI/Input/Input';
-import './Vocabulary.css'
+import Table from "../../../components/UI/Table/Table";
+import Input from "../../../components/UI/Input/Input";
+import "./Vocabulary.css";
 
 class Vocabulary extends Component {
-
   state = {
     users: [],
-    vocab:[],
+    vocab: [],
     vocabForm: {
       word: {
-        elementType: 'input',
+        elementType: "input",
         elementConfig: {
-          type: 'text',
-          placeholder: 'New word'
+          type: "text",
+          placeholder: "New word",
         },
-        value: '',
+        value: "",
         validation: {
           valid: false,
           required: true,
           unique: true,
           minLength: 1,
-          maxLength: 20
+          maxLength: 20,
         },
-        touched: false
+        touched: false,
       },
       translation: {
-        elementType: 'input',
+        elementType: "input",
         elementConfig: {
-          type: 'text',
-          placeholder: 'Translation'
+          type: "text",
+          placeholder: "Translation",
         },
-        value: '',
+        value: "",
         validation: {
           valid: false,
           required: true,
           minLength: 1,
-          maxLength: 40
+          maxLength: 40,
         },
-        touched: false
-      }
+        touched: false,
+      },
     },
-    formIsValid: false
-  }
+    formIsValid: false,
+  };
 
   componentDidMount() {
-    const user_id = 1;
-    fetch(`http://localhost:3001/users/${user_id}/vocabulary`)
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        }
-        throw new Error("Network response wasn't ok")
+    axios
+      .get("/vocabulary", {
+        headers: {
+          Authorization: `Bearer ${this.props.accessToken}`,
+        },
       })
-      .then(data => this.setState({ vocab: data }))
+      .then((response) => {
+        this.setState({ vocab: response.data });
+      })
+      .catch((error) => console.log(error));
   }
 
   checkValidity(value, rules) {
@@ -65,7 +67,7 @@ class Vocabulary extends Component {
     }
 
     if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
+      isValid = value.trim() !== "" && isValid;
     }
 
     if (rules.minLength) {
@@ -83,91 +85,96 @@ class Vocabulary extends Component {
     event.preventDefault();
 
     // need to check if word is unique in users vocab list before submitting
-    
+
     const vocabData = {
       user_id: 1,
       word: this.state.vocabForm.word.value,
-      word_language: 'spanish',
+      word_language: "spanish",
       translation: this.state.vocabForm.translation.value,
-      translation_language: 'english'
-    }
+      translation_language: "english",
+    };
 
-    fetch('http://localhost:3001/vocabulary', {
-      method: 'POST',
-      headers: {'Content-Type':'application/json', 'Accept': 'application/json'},
-      body:JSON.stringify(vocabData)
+    fetch("http://localhost:3001/vocabulary", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(vocabData),
     })
-    .then(response => {
-      console.log(response)
-      if (response.ok) {
-        return(response.text())
-      }
-      throw new Error("Network response wasn't ok")
-    })
-    .then(text => {
-      let updatedForm = {
-        ...this.state.vocabForm,
-      };
-      let updatedFormWord = {
-        ...updatedForm.word,
-        value: ''
-      };
-      let updatedFormTranslation = {
-        ...updatedForm.translation,
-        value: ''
-      };
-      updatedForm.word = updatedFormWord
-      updatedForm.translation = updatedFormTranslation
+      .then((response) => {
+        console.log(response);
+        if (response.ok) {
+          return response.text();
+        }
+        throw new Error("Network response wasn't ok");
+      })
+      .then((text) => {
+        let updatedForm = {
+          ...this.state.vocabForm,
+        };
+        let updatedFormWord = {
+          ...updatedForm.word,
+          value: "",
+        };
+        let updatedFormTranslation = {
+          ...updatedForm.translation,
+          value: "",
+        };
+        updatedForm.word = updatedFormWord;
+        updatedForm.translation = updatedFormTranslation;
 
-      console.log(updatedForm)
+        console.log(updatedForm);
 
-      this.setState({ vocabForm: updatedForm })
-      // console.log(text)
-    })
-
-  }
+        this.setState({ vocabForm: updatedForm });
+        // console.log(text)
+      });
+  };
 
   inputChangedHandler = (event, inputIdentifier) => {
     // console.log(event.target.value);
     const updatedVocabForm = {
-      ...this.state.vocabForm
-    }
+      ...this.state.vocabForm,
+    };
 
     const updatedFormElement = {
-      ...updatedVocabForm[inputIdentifier]
-    }
+      ...updatedVocabForm[inputIdentifier],
+    };
 
     const updatedFormValidity = {
-      ...updatedFormElement['validation']
-    }
+      ...updatedFormElement["validation"],
+    };
 
     updatedFormElement.value = event.target.value;
     updatedFormElement.touched = true;
-    updatedFormValidity.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
-    
-    updatedFormElement['validation'] = updatedFormValidity
+    updatedFormValidity.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
+
+    updatedFormElement["validation"] = updatedFormValidity;
     updatedVocabForm[inputIdentifier] = updatedFormElement;
-    
+
     let formIsValid = true;
     for (let inputIdentifier in updatedVocabForm) {
-      formIsValid = updatedVocabForm[inputIdentifier]['validation'].valid && formIsValid;
+      formIsValid =
+        updatedVocabForm[inputIdentifier]["validation"].valid && formIsValid;
     }
 
-    this.setState({ vocabForm: updatedVocabForm, formIsValid: formIsValid })
-  }
+    this.setState({ vocabForm: updatedVocabForm, formIsValid: formIsValid });
+  };
 
-  render () {
-
+  render() {
     const headings = [
-      'Spanish Word',
-      'Translation',
-      'Groups',
-      'Learning Status'
+      "Spanish Word",
+      "Translation",
+      "Groups",
+      "Learning Status",
     ];
 
-    const rows = this.state.vocab.map(row => {
-      return [row.word, row.translation, '', row.progress]
-    })
+    const rows = this.state.vocab.map((row) => {
+      return [row.word, row.translation, "", row.progress];
+    });
     // const rows = [
     //   [
     //     'hola',
@@ -217,30 +224,41 @@ class Vocabulary extends Component {
       <>
         <h1>Vocabulary List</h1>
         <form onSubmit={this.addVocabHandler}>
-          <Input 
-            type='text'
-            name='word'
+          <Input
+            type="text"
+            name="word"
             placeholder={"Enter new word"}
             value={this.state.vocabForm.word.value}
-            onChange={(event) => this.inputChangedHandler(event, 'word')}
-            invalid={!this.state.vocabForm.word.validation.valid && this.state.vocabForm.word.touched}
+            onChange={(event) => this.inputChangedHandler(event, "word")}
+            invalid={
+              !this.state.vocabForm.word.validation.valid &&
+              this.state.vocabForm.word.touched
+            }
           />
-          <Input 
-            type='text'
-            name='translation'
+          <Input
+            type="text"
+            name="translation"
             placeholder={"Enter translation"}
             value={this.state.vocabForm.translation.value}
-            onChange={(event) => this.inputChangedHandler(event, 'translation')}
-            invalid={!this.state.vocabForm.translation.validation.valid && this.state.vocabForm.translation.touched}
+            onChange={(event) => this.inputChangedHandler(event, "translation")}
+            invalid={
+              !this.state.vocabForm.translation.validation.valid &&
+              this.state.vocabForm.translation.touched
+            }
           />
           <button disabled={!this.state.formIsValid}>Submit</button>
         </form>
 
         <Table headings={headings} rows={rows} />
-
       </>
     );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    accessToken: state.token,
   };
 };
 
-export default Vocabulary;
+export default connect(mapStateToProps)(Vocabulary);
