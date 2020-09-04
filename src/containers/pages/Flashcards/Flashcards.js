9354c9911a650b341624bd01fac97896d0f21d3a
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-04 11:56:08 +0100

    Imported axios into Flashcard component and replaced vocab count fetch with axios

--- src/containers/pages/Flashcards/Flashcards.js
+++ src/containers/pages/Flashcards/Flashcards.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import Flashcard from '../../../components/Flashcard/Flashcard';
-import FlashcardSummary from '../../../components/Flashcard/Summary/FlashcardSummary';
+import Flashcard from "../../../components/Flashcard/Flashcard";
+import FlashcardSummary from "../../../components/Flashcard/Summary/FlashcardSummary";
 
 const MINIMUM_FLASHCARDS = 5;
 const MAXIMUM_FLASHCARDS = 30;
 
 class Flashcards extends Component {
-
   state = {
     numFlashcards: MINIMUM_FLASHCARDS,
     countVocabList: 0,
@@ -17,95 +17,115 @@ class Flashcards extends Component {
     progress: {
       numCorrect: 0,
       numIncorrect: 0,
-      numRemaining: 0
+      numRemaining: 0,
     },
     flipped: false,
     transition: true,
-    showSummary: false
-  }
+    showSummary: false,
+  };
 
-  componentDidMount () {
-    const user_id = 1;
-    fetch(`http://localhost:3001/users/${user_id}/vocabulary/count`)
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        }
-        throw new Error("Network response wasn't ok")
+  componentDidMount() {
+    // const user_id = 1;
+    axios
+      .get("/vocabulary/count", {
+        headers: {
+          Authorization: `Bearer ${this.props.accessToken}`,
+        },
       })
-      .then(data => this.setState({ countVocabList: data }))
+      .then((response) => {
+        this.setState({ countVocabList: response.data });
+      })
+      .catch((error) => console.log(error));
+
+    // fetch(`http://localhost:3001/users/${user_id}/vocabulary/count`)
+    //   .then((response) => {
+    //     if (response.ok) {
+    //       return response.json();
+    //     }
+    //     throw new Error("Network response wasn't ok");
+    //   })
+    //   .then((data) => this.setState({ countVocabList: data }));
   }
 
   increaseFlashcardsHandler = () => {
-    if (this.state.numFlashcards < Math.min(MAXIMUM_FLASHCARDS, this.state.countVocabList)) {
-      this.setState(prevState => {
-        return { numFlashcards: prevState.numFlashcards + 1 }
-      })
+    if (
+      this.state.numFlashcards <
+      Math.min(MAXIMUM_FLASHCARDS, this.state.countVocabList)
+    ) {
+      this.setState((prevState) => {
+        return { numFlashcards: prevState.numFlashcards + 1 };
+      });
     }
-  }
+  };
 
   decreaseFlashcardsHandler = () => {
     if (this.state.numFlashcards > MINIMUM_FLASHCARDS) {
-      this.setState(prevState => {
-        return { numFlashcards: prevState.numFlashcards - 1 }
-      })
+      this.setState((prevState) => {
+        return { numFlashcards: prevState.numFlashcards - 1 };
+      });
     }
-  }
+  };
 
   startFlashcardsHandler = () => {
-
     const flashcardRequirements = {
       user_id: 1,
-      number: this.state.numFlashcards
-    }
+      number: this.state.numFlashcards,
+    };
 
-    fetch('http://localhost:3001/flashcards', {
-      method: 'POST',
-      headers: {'Content-Type':'application/json', 'Accept': 'application/json'},
-      body:JSON.stringify(flashcardRequirements)
+    fetch("http://localhost:3001/flashcards", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(flashcardRequirements),
     })
-    .then(response => {
-      console.log(response)
-      if (response.ok) {
-        return(response.json())
-      }
-      throw new Error("Network response wasn't ok")
-    })
-    .then(data => {
-      let vocabData = []
-      data.map(vocab => {
-        let updatedVocab = {
-          ...vocab,
-          seen: false,
-          correct: false,
-          incorrect: false
+      .then((response) => {
+        console.log(response);
+        if (response.ok) {
+          return response.json();
         }
-        return vocabData.push(updatedVocab)
+        throw new Error("Network response wasn't ok");
       })
-      const updatedProgress = this.updateProgress(vocabData)
-      this.setState({ flashcardsVocab: vocabData, showFlashcards: true, progress: updatedProgress })
-    })
-  }
+      .then((data) => {
+        let vocabData = [];
+        data.map((vocab) => {
+          let updatedVocab = {
+            ...vocab,
+            seen: false,
+            correct: false,
+            incorrect: false,
+          };
+          return vocabData.push(updatedVocab);
+        });
+        const updatedProgress = this.updateProgress(vocabData);
+        this.setState({
+          flashcardsVocab: vocabData,
+          showFlashcards: true,
+          progress: updatedProgress,
+        });
+      });
+  };
   updateProgress = (vocabList) => {
     let updatedProgress = {
       numCorrect: 0,
       numIncorrect: 0,
-      numRemaining: this.state.numFlashcards
-    }
-    vocabList.map(vocab => {
+      numRemaining: this.state.numFlashcards,
+    };
+    vocabList.map((vocab) => {
       if (vocab.seen) {
-        updatedProgress.numRemaining--
+        updatedProgress.numRemaining--;
       }
       if (vocab.correct) {
-        updatedProgress.numCorrect++
+        updatedProgress.numCorrect++;
       }
       if (vocab.incorrect) {
-        updatedProgress.numIncorrect++
+        updatedProgress.numIncorrect++;
       }
-      return 'done';
-    })
-    return updatedProgress
-  }
+      return "done";
+    });
+    return updatedProgress;
+  };
 
   correctHandler = () => {
     const updatedVocabList = this.state.flashcardsVocab.map((vocab, index) => {
@@ -114,14 +134,17 @@ class Flashcards extends Component {
           ...vocab,
           seen: true,
           correct: true,
-          incorrect: false
-        }
+          incorrect: false,
+        };
       }
-      return vocab
-    })
-    const updatedProgress = this.updateProgress(updatedVocabList)
-    this.setState({ flashcardsVocab: updatedVocabList, progress: updatedProgress })
-  }
+      return vocab;
+    });
+    const updatedProgress = this.updateProgress(updatedVocabList);
+    this.setState({
+      flashcardsVocab: updatedVocabList,
+      progress: updatedProgress,
+    });
+  };
 
   incorrectHandler = () => {
     const updatedVocabList = this.state.flashcardsVocab.map((vocab, index) => {
@@ -130,105 +153,143 @@ class Flashcards extends Component {
           ...vocab,
           seen: true,
           correct: false,
-          incorrect: true
-        }
+          incorrect: true,
+        };
       }
-      return vocab
-    })
-    const updatedProgress = this.updateProgress(updatedVocabList)
-    this.setState({ flashcardsVocab: updatedVocabList, progress: updatedProgress })
-  }
+      return vocab;
+    });
+    const updatedProgress = this.updateProgress(updatedVocabList);
+    this.setState({
+      flashcardsVocab: updatedVocabList,
+      progress: updatedProgress,
+    });
+  };
 
   nextFlashcardHandler = () => {
-    this.setState(prevState => {
-      return { showFlashcardNumber: prevState.showFlashcardNumber + 1, flipped: false, transition: false }
-    })
-  }
+    this.setState((prevState) => {
+      return {
+        showFlashcardNumber: prevState.showFlashcardNumber + 1,
+        flipped: false,
+        transition: false,
+      };
+    });
+  };
 
   previousFlashcardHandler = () => {
-    this.setState(prevState => {
-      return { showFlashcardNumber: prevState.showFlashcardNumber - 1, flipped: false, transition: false }
-    })
-  }
+    this.setState((prevState) => {
+      return {
+        showFlashcardNumber: prevState.showFlashcardNumber - 1,
+        flipped: false,
+        transition: false,
+      };
+    });
+  };
 
   flipHandler = () => {
-    this.setState(prevState => ({
+    this.setState((prevState) => ({
       flipped: !prevState.flipped,
-      transition: true
+      transition: true,
     }));
   };
 
   flashcardFinishHandler = () => {
-    fetch('http://localhost:3001/flashcards/completed', {
-      method: 'POST',
-      headers: {'Content-Type':'application/json', 'Accept': 'application/json'},
-      body:JSON.stringify(this.state.flashcardsVocab)
+    fetch("http://localhost:3001/flashcards/completed", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(this.state.flashcardsVocab),
     })
-    .then(response => {
-      console.log(response)
-      if (response.ok) {
-        return(response.text())
-      }
-      throw new Error("Network response wasn't ok")
-    })
-    .then(text => {
-      console.log(text)
-    })
-    this.setState({ showSummary: true, showFlashcards: false })
-  }
+      .then((response) => {
+        console.log(response);
+        if (response.ok) {
+          return response.text();
+        }
+        throw new Error("Network response wasn't ok");
+      })
+      .then((text) => {
+        console.log(text);
+      });
+    this.setState({ showSummary: true, showFlashcards: false });
+  };
 
   startAgainHandler = () => {
-    this.setState({ showSummary: false, showFlashcards: false, showFlashcardNumber: 0 })
-  }
+    this.setState({
+      showSummary: false,
+      showFlashcards: false,
+      showFlashcardNumber: 0,
+    });
+  };
 
-  render () {
-
-    const nextDisabled = this.state.numFlashcards === this.state.showFlashcardNumber + 1;
+  render() {
+    const nextDisabled =
+      this.state.numFlashcards === this.state.showFlashcardNumber + 1;
     const previousDisabled = this.state.showFlashcardNumber === 0;
 
     return (
       <>
         <h1>Flashcards page</h1>
-        {
-          this.state.countVocabList > 5 && !this.state.showFlashcards && !this.state.showSummary
-          ?
+        {this.state.countVocabList > 5 &&
+        !this.state.showFlashcards &&
+        !this.state.showSummary ? (
           <>
             <div>{this.state.numFlashcards}</div>
             <button onClick={this.decreaseFlashcardsHandler}>-</button>
             <button onClick={this.increaseFlashcardsHandler}>+</button>
             <button onClick={this.startFlashcardsHandler}>start</button>
           </>
-          :
-            this.state.showFlashcards
-          ?
-            <>
-              <p>{this.state.progress.numCorrect} correct, {this.state.progress.numIncorrect} incorrect. {this.state.progress.numRemaining} Flashcards to go</p>
-              <Flashcard 
-                front={this.state.flashcardsVocab[this.state.showFlashcardNumber].word}
-                back={this.state.flashcardsVocab[this.state.showFlashcardNumber].translation}
-                flipped={this.state.flipped}
-                clicked={this.flipHandler}
-                transition={this.state.transition}
-              />
-              <button onClick={this.correctHandler}>Correct</button>
-              <button onClick={this.incorrectHandler}>Incorrect</button>
-              <button onClick={this.previousFlashcardHandler} disabled={previousDisabled}>Previous</button>
-              <button onClick={this.nextFlashcardHandler} disabled={nextDisabled || !this.state.flashcardsVocab[this.state.showFlashcardNumber].seen}>Next</button>
-              <button onClick={this.flashcardFinishHandler}>Finish</button>
-            </>
-          :
-            this.state.showSummary
-            ?
-            <>
-              <FlashcardSummary summary={this.state.flashcardsVocab} />
-              <button onClick={this.startAgainHandler}>Start Again</button>
-            </>
-            :
-          <p>You need to have at least 5 words in your vocab list. Please add more words on the Vocabulary page</p>
-        }
-
+        ) : this.state.showFlashcards ? (
+          <>
+            <p>
+              {this.state.progress.numCorrect} correct,{" "}
+              {this.state.progress.numIncorrect} incorrect.{" "}
+              {this.state.progress.numRemaining} Flashcards to go
+            </p>
+            <Flashcard
+              front={
+                this.state.flashcardsVocab[this.state.showFlashcardNumber].word
+              }
+              back={
+                this.state.flashcardsVocab[this.state.showFlashcardNumber]
+                  .translation
+              }
+              flipped={this.state.flipped}
+              clicked={this.flipHandler}
+              transition={this.state.transition}
+            />
+            <button onClick={this.correctHandler}>Correct</button>
+            <button onClick={this.incorrectHandler}>Incorrect</button>
+            <button
+              onClick={this.previousFlashcardHandler}
+              disabled={previousDisabled}
+            >
+              Previous
+            </button>
+            <button
+              onClick={this.nextFlashcardHandler}
+              disabled={
+                nextDisabled ||
+                !this.state.flashcardsVocab[this.state.showFlashcardNumber].seen
+              }
+            >
+              Next
+            </button>
+            <button onClick={this.flashcardFinishHandler}>Finish</button>
+          </>
+        ) : this.state.showSummary ? (
+          <>
+            <FlashcardSummary summary={this.state.flashcardsVocab} />
+            <button onClick={this.startAgainHandler}>Start Again</button>
+          </>
+        ) : (
+          <p>
+            You need to have at least 5 words in your vocab list. Please add
+            more words on the Vocabulary page
+          </p>
+        )}
       </>
-    )
+    );
   }
 }
 
