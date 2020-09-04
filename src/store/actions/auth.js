4346709ff6c5b2d50bb789b151a0e6b24a78b0ca
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-04 17:09:30 +0100

    Save accessToken and expirationDate to local storage when user logs in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,58 +1,64 @@
-import * as actionTypes from './actionTypes';
-import axios from 'axios';
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
 
 export const authStart = () => {
-    return {
-        type: actionTypes.AUTH_START
-    };
+  return {
+    type: actionTypes.AUTH_START,
+  };
 };
 
 export const authSuccess = (token, userId) => {
-    return {
-        type: actionTypes.AUTH_SUCCESS,
-        accessToken: token,
-        userId: userId
-    };
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    accessToken: token,
+    userId: userId,
+  };
 };
 
 export const authFail = (error) => {
-    return {
-        type: actionTypes.AUTH_FAIL,
-        error: error
-    };
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
 };
 
 export const logout = () => {
-    return {
-        type: actionTypes.AUTH_LOGOUT
-    }
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
 };
 
 export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout())
-        }, expirationTime * 1000)
-    };
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
 };
 
 export const auth = (userDetails, isRegistering) => {
-    return dispatch => {
-        dispatch(authStart());
+  return (dispatch) => {
+    dispatch(authStart());
 
-        let url = 'users/login';
-        if (isRegistering) {
-            url = '/users/register';
-        }
-        axios.post(url, userDetails)
-        .then(response => {
-            console.log(response)
-            dispatch(authSuccess(response.data.accessToken, response.data.userId))
-            dispatch(checkAuthTimeout(response.data.expiresIn))
-        })
-        .catch(error => {
-            console.log(error.response)
-            dispatch(authFail(error.response.data.error))
-        })
-    };
+    let url = "users/login";
+    if (isRegistering) {
+      url = "/users/register";
+    }
+    axios
+      .post(url, userDetails)
+      .then((response) => {
+        console.log(response);
+        const expirationDate = new Date(
+          new Date().getTime() + response.data.expiresIn * 1000
+        );
+        localStorage.setItem("accessToken", response.data.accessToken);
+        localStorage.setItem("expirationDate", expirationDate);
+        dispatch(authSuccess(response.data.accessToken, response.data.userId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
+      })
+      .catch((error) => {
+        console.log(error.response);
+        dispatch(authFail(error.response.data.error));
+      });
+  };
 };
