7daafaed7b554963f602f83645d563034958d0b9
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-04 15:05:18 +0100

    Dispatch logout action when Logout component did mount

--- src/containers/pages/Authentication/Logout.js
+++ src/containers/pages/Authentication/Logout.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import * as actions from '../../../store/actions/index';
 
 class Logout extends Component {
+    componentDidMount() {
+        this.props.onLogout();
+    }
+
     render() {
         return ();
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onLogout: () => dispatch(actions.logout())
     }
 }
 
