835009a573d0c22f29afe3fd8d082e69029519bf
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-08 16:37:53 +0100

    Added a passing test

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import thunk from "redux-thunk";
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+import authReducer from "../store/reducers/auth";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  const store = createStore(authReducer, applyMiddleware(thunk));
+  const { getByText } = render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+  const linkElement = getByText(/Home page/i);
+  expect(linkElement).toBeInTheDocument();
+});
