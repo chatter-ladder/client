ce95cadf133c601e6e9a66bf83bcf318191d2965
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-10 16:41:30 +0100

    Updated Spinner to have css animation

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,7 +1,8 @@
 import React from "react";
+import classes from "./Spinner.module.css";
 
 const Spinner = () => {
-  return <div>Loading...</div>;
+  return <div className={classes.Loader}>Loading...</div>;
 };
 
 export default Spinner;
